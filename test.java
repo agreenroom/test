b7772d06c3f4af7553bdf0b8bb236e321f9d5da4
agreenroom <tjwlals7805@gmail.com>
2025-05-17 15:11:10 +0900

    first commit

--- test.java
+++ test.java
@@ -1,11 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class text {
+public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i =0;
