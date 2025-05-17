c0430f03c865bacf2b366ef71f033fe027b412e5
agreenroom <tjwlals7805@gmail.com>
2025-05-17 16:15:14 +0900

    add Main2

---
+++ collection/array/ArrayMain2.java
@@ -0,0 +1,14 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2{
+
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
