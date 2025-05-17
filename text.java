7bb3c1dae6b147d1ee664981f08aa72d23d5c92d
agreenroom <tjwlals7805@gmail.com>
2025-05-17 14:36:09 +0900

    아아

--- text.java
+++ text.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class text {
@@ -10,4 +15,58 @@ public class text {
             i++;
         }
     }
+
+    public void updateMember(MemberVO vo){
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        MemberVO vo = null;
+
+        try {
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", 
+        "scott", "tiger");
+        String sql = "select * from student where id=?";
+        pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, "id");
+        rs = pstmt.executeQuery();
+
+        if(rs.next()){
+            vo = new MemberVO();
+            vo.setId(rs.getString("id"));
+            vo.setPass(rs.getString("pass"));
+            vo.setZip(rs.getString("zip"));
+            vo.setAddress(rs.getString("address"));
+        }
+        
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+
+        finally{
+            if(con!=null){
+                try {
+                    con.close();
+                } catch (SQLException ss) {
+                    ss.printStackTrace();
+                }
+            }
+                if(pstmt!=null){
+                try {
+                    pstmt.close();
+                } catch (SQLException ss) {
+                    ss.printStackTrace();
+                }
+            }
+                if(rs!=null){
+                try {
+                    rs.close();
+                } catch (SQLException ss) {
+                    ss.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
