356ba54f676aba3bbee846bd72ab9d1be4e9ded9
agreenroom <tjwlals7805@gmail.com>
2025-05-17 14:56:50 +0900

    first commit

---
+++ MemberVO.java
@@ -0,0 +1,33 @@
+public class MemberVO {
+    private String id;
+    private String pass;
+    private String zip;
+    private String address;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
+}
