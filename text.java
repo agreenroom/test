a972b4aa95e286002843dd2c5cf847440d76c748
agreenroom <tjwlals7805@gmail.com>
2025-05-17 14:23:56 +0900

    어디서 실행하는 거임..

--- text.java
+++ text.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class text {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int i =0;
+        while(i<4){
+            String message = sc.nextLine();
+            System.out.println("hello, i'm "+message);
+            i++;
+        }
     }
 }
